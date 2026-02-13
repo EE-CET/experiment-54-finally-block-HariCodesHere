bb8a41a6e53bbde3ff49e9ab641d2b942f1e1c01
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-13 09:59:20 +0530

    imple : dev

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -9,13 +9,14 @@ public class FinallyDemo {
 
                 try{
                         int c = a/b;
-                        System.out.println(c);
+                        System.out.println("Result: "+c);
                 }
                 catch(ArithmeticException ex){
-                        System.out.println("Divide by zero error");
+                        System.out.println("Error: " + ex.getMessage() );
                 }
                 finally {
                         System.out.println("Finally block executed");
                 }
         }
 }
+
